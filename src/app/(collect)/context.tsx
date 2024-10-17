27471c566d607d96aeedc0fa878d67a0136ad3db
Drew Beamer <andrewmbeamer@gmail.com>
2024-10-16 21:20:03 -0400

    chore: remove unused imports

--- src/app/(collect)/context.tsx
+++ src/app/(collect)/context.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { RequiredMetadata } from "@/lib/components/ClickableFieldMap/ClickableFieldMap";
-import { Position } from "@/lib/types";
-import { createContext, ReactElement, useContext } from "react";
+import { useContext } from "react";
 import { ClickMapCollectionContext } from "./layout";
 
 export const useClickMapContext = () => useContext(ClickMapCollectionContext);
